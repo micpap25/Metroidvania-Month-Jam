b66dc6e497f442599b8eefe6cf107f2f7983f3c5
Michael Papadopoulos <micpap25@gmail.com>
2020-05-18 12:08:18 -0400

    Player & Enemy Outline Started

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    public int health, damage, knockback, stun, inv;
+
+    
+
+    public void TakeDamage(int _damage) {
+        health -= _damage;
+        //change sprite to stunned and flicker it
+        
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+
+    public bool hasSword, hasSpear, hasAx, hasBow, hasBoots, hasGloves, hasRope, hasDash = false;
+    public int attLvl, dexLvl, invLvl, spdLvl = 1;
+
+    public int speed, jumpHeight, health, knockback, stun, dmg, invTime;
+    public int[] attackFrames;
+
+    private bool isGrounded, isAttacking, isHitstun, isInv;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    public IEnumerator Attack()
+    {
+        return null;
+    }
+
+    public void TakeDamage()
+    {
+
+    }
+
+    
+}
